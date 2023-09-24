4dd7af3cca067237b43207824126784865d3a14e
Khang Nguyen <nguyendakcun@gmail.com>
2023-09-24 20:21:00 +0700

    fix bug square

--- shape.py
+++ shape.py
@@ -1,25 +1,36 @@
 import math
 class Shape:
-    def __init__(self,color,shape):
+    def __init__(self,shape = 'shape'):
         self.shape = shape
-        self.color = color
+        self.color = 'red'
 class Circle(Shape):
-    def __init__(self,radius,color,shape):
-        super().__init__(color,shape)
+    def __init__(self,radius):
+        super().__init__(shape = 'circle')
         self.radius = radius
     def get_area(self):
         return math.pi * self.radius ** 2
 class Rectangle(Shape):
-    def __init__(self,width,height,color,shape):
-        super().__init__(color,shape)
+    def __init__(self,width,height,shape = 'Rectangle'):
+        super().__init__(shape = shape)
         self.width = width
         self.height = height
     def get_area(self):
         return self.width * self.height
-class Rectangle(Shape):
-    def __init__(self,width,height,color,shape):
-        super().__init__(color,shape)
-        self.width = width
+class Square(Rectangle):
+    def __init__(self,height,width):
+        super().__init__(width= height,height = height,shape ='Square')
         self.height = height
     def get_area(self):
-        return self.width * self.height
+        return self.height * self.height 
+circle = Circle(1)
+
+print(circle.get_area())
+# Output: 314.1592653589793
+
+rectangle = Rectangle(10, 20)
+print(rectangle.get_area())
+# Output: 200
+
+square = Square(10)
+print(square.get_area())
+# Output: 100
