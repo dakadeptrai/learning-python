83507aa3f431f365accc6bb30ed6587177792aaa
Khang Nguyen <nguyendakcun@gmail.com>
2023-10-01 20:54:19 +0700

    check xem co win chua

--- tube.py
+++ tube.py
@@ -19,7 +19,7 @@ class Tube(StackLL):
         if self.length == 0:
             return True
         return False
-    def checkempty(self):
+    def checkfull(self):
         if self.length == self.buffer_size:
             return True
         return False
@@ -38,5 +38,38 @@ class TubeManager():
     def __init__(self,tubelist : list[Tube] = []):
         self.tubelist = tubelist
         self.status = [False]*len(tubelist)
-
+    def isValidMove(self,scrTube : Tube,desTube : Tube):
+        if (not scrTube.checkempty()) and (not desTube.checkfull()):
+            if desTube.checkempty():
+                return True
+            elif scrTube.getTopColor().data==desTube.getTopColor().data:
+                return True
+            else:
+                return False
+        return False
+    def moveWater(self,srcIndex,desIndex):
+        scrTube = self.tubelist[srcIndex]
+        desTube = self.tubelist[desIndex]
+        if self.isValidMove(scrTube,desTube):
+            color = scrTube.pop()
+            desTube.push(color)
+    def isFinish(self):
+        for i in range(len(self.tubelist)):
+            tubeitem = self.tubelist[i]
+            if tubeitem.checkempty():
+                self.status[i] = True
+            elif tubeitem.checkfull():
+                self.status[i] = True
+                colors = tubeitem.getListColor()
+                waterA = colors[0]
+                for waterB in colors[1:]:
+                    if waterA != waterB:
+                        self.status[i] = False
+            else:
+                self.status[i] = False
+        for s in self.status:
+            if s == False:
+                print('You didnt win')
+                return False
+        print('You Win')
 
